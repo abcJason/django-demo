8e195081b6c3aec7e10564e73f4a9ea4d3222d63
jason <abcjason110@gmail.com>
2024-09-28 13:29:16 +0800

    註冊所有功能完成

--- user/views.py
+++ user/views.py
@@ -1,18 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
+
+
+def user_profile(request):
+    return render(request, "user/profile.html")
+
 
 # Create your views here.
+def user_login(request):
+    message = ""
+    user = None
+    username=request.session.get("username","")
+
+    if request.method == "POST":
+        if request.POST.get("register"):
+            return redirect("register")
+
+        if request.POST.get("login"):
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+
+            if username == "" or password == "":
+                message = "帳號密碼不能為空!"
+            else:
+                user = authenticate(request, username=username, password=password)
+                if user:
+                    message = "登入成功!"
+                    login(request, user)
+                    return redirect("profile")
+                else:
+                    message = "帳號或密碼錯誤!"
+
+    return render(request, "user/login.html", {"message": message, "user": user, "username":username})
 
 
-def index(request):
+def user_register(request):
     message = ""
     form = UserCreationForm()
-
-    # all , get , filter
+    # all,get,filter
     print(User.objects.all())
-    # print(User.objects.get(username='jason'))
-    # print(User.objects.filter(username='jason1'))
 
     if request.method == "POST":
         print(request.POST)
@@ -21,21 +54,20 @@ def index(request):
         password2 = request.POST.get("password2")
 
         # 密碼長度8
-        if len(password1) != 8 or len(password2) != 8:
+        if len(password1) < 8 or len(password2) < 8:
             message = "密碼長度不正確"
         elif password1 != password2:
             message = "兩次密碼不一樣"
         else:
-
-        # 密碼相同
-
             # 比對使用者是否存在
             if User.objects.filter(username=username):
-                message = "帳號已存在!!!"             
+                message = "帳號已存在!"
             # 註冊使用者
             else:
-                user=User.objects.create_user(username=username,password=password1)
+                user = User.objects.create_user(username=username, password=password1)
                 user.save()
                 message = "註冊成功!"
+                request.session["username"]=user.username
+                return redirect("login")
 
     return render(request, "user/register.html", {"form": form, "message": message})
