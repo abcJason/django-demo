3d59eb5bcbd383c201e8c85f26b634c25dba54a5
jason <abcjason110@gmail.com>
2024-09-28 09:43:01 +0800

    註冊功能完成

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -7,6 +8,11 @@ from django.contrib.auth.forms import UserCreationForm
 def index(request):
     message = ""
     form = UserCreationForm()
+
+    # all , get , filter
+    print(User.objects.all())
+    # print(User.objects.get(username='jason'))
+    # print(User.objects.filter(username='jason1'))
 
     if request.method == "POST":
         print(request.POST)
@@ -19,11 +25,17 @@ def index(request):
             message = "密碼長度不正確"
         elif password1 != password2:
             message = "兩次密碼不一樣"
+        else:
 
         # 密碼相同
 
-        # 比對使用者是否存在
-
-        # 註冊使用者
+            # 比對使用者是否存在
+            if User.objects.filter(username=username):
+                message = "帳號已存在!!!"             
+            # 註冊使用者
+            else:
+                user=User.objects.create_user(username=username,password=password1)
+                user.save()
+                message = "註冊成功!"
 
     return render(request, "user/register.html", {"form": form, "message": message})
